119748010a755dbac8e6dfe5db3ecde948e82540
Arman2232 <82290609+Arman2232@users.noreply.github.com>
2021-05-22 19:31:08 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setFirstName("Arman");
-        person.setLastName("Hambardzumyan");
-        person.setAge(19);
-        person.setSex("male");
-        person.setPassportId("123456");
-
-//        System.out.println(person.getFirstName());
-//        System.out.println(person.getLastName());
-//        System.out.println(person.getAge());
-//        System.out.println(person.getSex());
-//        System.out.println(person.getPassportId());
-
+        Person person = new Person("Arman", "Hambardzumyan", 19, "male", "123456");
         System.out.println(person.toString());
     }
 }
